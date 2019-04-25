8a532bfd46433f375f1a8c8ab88c1bebb4f3b8f4
132ikl <132ikl@gmail.com>
2019-04-25 17:18:12 -0400

    Inital commit

---
+++ app/src/main/java/sh/ikl/liteshort/MainActivity.java
@@ -0,0 +1,21 @@
+package sh.ikl.liteshort;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Snackbar.make(v, "YEET", 1000).show();
+    }
+
+}
